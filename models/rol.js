3eb2b4798dd888c7818b2230dc0f758a0dc89053
EALeon16 <eduardo.leon@unl.edu.ec>
2019-07-31 13:02:06 -0500

    EALeon16

---
+++ models/rol.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const rol = sequelize.define('rol', {
+    tipo: DataTypes.STRING
+  }, {});
+  rol.associate = function(models) {
+    // associations can be defined here
+  };
+  return rol;
+};
