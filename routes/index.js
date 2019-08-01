6dc67c111cf8b3f14fd216bf868a26667c06b7dc
Israel34 <israelcampoverfe@gmail.com>
2019-08-01 00:27:22 -0500

    commit3

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+//persona controlador 
+var persona = require('../controladores/persona_controller');
+var Persona = new persona();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'AMovie'});
@@ -13,5 +18,8 @@ router.get('/', function (req, res, next) {
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'AMovie' });
 });
+
+
+router.post('/persona_controller', Persona.guardar);
 
 module.exports = router;
