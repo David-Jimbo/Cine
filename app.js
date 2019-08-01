aea4775df8cd09e30ae878ca37d26037dceb2bec
David-Jimbo <rudi.jimbo@unl.edu.ec>
2019-07-31 21:01:55 -0500

    si

--- app.js
+++ app.js
@@ -22,22 +22,22 @@ app.use(bodyParser.json());
 // view engine setup
 app.set("views", "app/views");
 app.engine(
-  "hbs",
-  exphbs({
-    extname: ".hbs",
-    defaultLayout: "",
-    layoutsDir: "",
-    partialsDir: ["app/views"]
-  })
+    "hbs",
+    exphbs({
+        extname: ".hbs",
+        defaultLayout: "",
+        layoutsDir: "",
+        partialsDir: ["app/views"]
+    })
 );
 app.set("view engine", "hbs");
 
 app.use(
-  session({
-    secret: "cuarto-sistemas-A",
-    resave: true,
-    saveUninitialized: true
-  })
+    session({
+        secret: "cuarto-sistemas-A",
+        resave: true,
+        saveUninitialized: true
+    })
 );
 
 require("dotenv").config();
@@ -53,19 +53,19 @@ app.use("/", indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
